978f3ec22dfc45a27ff4cb9ae19f8b435472d04c
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-09-10 17:51:09 -0400

    :wrench: Add roboto

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import Router from "./Router";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+import "typeface-roboto";
 
 ReactDOM.render(<Router />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
